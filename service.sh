249d1c00817d00e34c6cfd3c13d7b8e0af630f43
bangsawan02 <telo.kuh@gmail.com>
2026-06-03 19:09:08 +0700

    ci: update service.sh

--- service.sh
+++ service.sh
@@ -1,21 +1,16 @@
 #/system/bin/sh
-# ksu-frida Stealth Daemon Boot launcher script
+# ksu-frida Stealth Gadget Boot launcher script
 MODDIR=${0%/*}
 FRIDA_DIR="/data/local/tmp/.aux_stealth"
 mkdir -p "$FRIDA_DIR"
 chmod 771 "$FRIDA_DIR"
 chown system:system "$FRIDA_DIR"
 
-# Read dynamic config
-CONFIG_FILE="$MODDIR/config.json"
-PORT=$(grep -o '"port":[0-9]*' "$CONFIG_FILE" | cut -d: -f2 || echo "27342")
-
-if [ -f "$MODDIR/bin/frida-server" ]; then
-  cp "$MODDIR/bin/frida-server" "$FRIDA_DIR/ksu_w_core"
-  chmod 755 "$FRIDA_DIR/ksu_w_core"
-  chcon u:object_r:system_file:s0 "$FRIDA_DIR/ksu_w_core"
-  
-  # Start the background proxy stealth daemon 
-  "$FRIDA_DIR/ksu_w_core" -D --listen 127.0.0.1:$PORT &
+# Move Gadget library to stealth realm
+if [ -f "$MODDIR/lib/frida-gadget.so" ]; then
+  cp "$MODDIR/lib/frida-gadget.so" "$FRIDA_DIR/ksu_w_core.so"
+  cp "$MODDIR/lib/frida-gadget.config" "$FRIDA_DIR/ksu_w_core.config"
+  chmod 755 "$FRIDA_DIR/ksu_w_core.so"
+  chcon u:object_r:system_file:s0 "$FRIDA_DIR/ksu_w_core.so"
   chmod 700 "$FRIDA_DIR"
 fi
